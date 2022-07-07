1e54ff994916ef3a7d5eef7a333b9067511fbe66
Carmen Chiu <carmenchiu@Carmens-iMac.local>
2022-07-07 13:55:51 -0700

    first commit

---
+++ Bookshelf/BookshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfApp.swift
+//  Bookshelf
+//
+//  Created by Carmen Chiu on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookshelf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookshelf
+//
+//  Created by Carmen Chiu on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
